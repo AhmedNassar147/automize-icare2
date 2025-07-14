b959927db60c70009364f161ee247157a3729ed3
AhmedNassar147 <nassarahmed750@gmail.com>
2025-07-14 23:21:43 +0300

    make app more faster

--- src/processClientActionOnPatient.mjs
+++ src/processClientActionOnPatient.mjs
@@ -202,7 +202,7 @@ const processClientActionOnPatient = async ({
 
   console.timeEnd("🕒 prepare_user_action_start_time");
 
-  const remainingTimeMS = caseActualWillBeSubmittedAtMS - Date.now() + 58;
+  const remainingTimeMS = caseActualWillBeSubmittedAtMS - Date.now() + 60;
 
   if (remainingTimeMS > 0) {
     console.log("remainingTimeMS to execute action: ", remainingTimeMS);
