1c6e1e020321530584194e1eeb22316c4626b487
AhmedNassar147 <nassarahmed750@gmail.com>
2026-05-12 13:36:58 +0300

    try new

--- src/handleCaseAcceptanceOrRejection.mjs
+++ src/handleCaseAcceptanceOrRejection.mjs
@@ -154,7 +154,7 @@ const handleCaseAcceptanceOrRejection =
             message: approvalMessage,
           }),
         ),
-        sleep(waitTime - 64).then(() => sendTelegramMessage(approvalMessage)),
+        sleep(waitTime - 62).then(() => sendTelegramMessage(approvalMessage)),
 
         sleep(waitTime - 34).then(() => sendNtfyMessage(approvalMessage)),
       ]);
