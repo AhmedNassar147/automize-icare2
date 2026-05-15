ab5ec7114f72c5974930216275d6fab81e9eade5
AhmedNassar147 <nassarahmed750@gmail.com>
2026-05-15 04:08:23 +0300

    fix sending excel files to telegram

--- src/getCurrentUserNtfyID.mjs
+++ src/getCurrentUserNtfyID.mjs
@@ -4,9 +4,15 @@
  *
  */
 const getCurrentUserNtfyID = () => {
-  const { NTFY_BASE_ID, CLIENT_WHATSAPP_NUMBER } = process.env;
+  const { NTFY_BASE_ID, CLIENT_WHATSAPP_NUMBER, TG_CHAT_ID } = process.env;
 
-  return `${CLIENT_WHATSAPP_NUMBER}_${NTFY_BASE_ID}`;
+  let phoneNumber = CLIENT_WHATSAPP_NUMBER;
+
+  if (TG_CHAT_ID === "6585807478") {
+    phoneNumber = process.env[`TG_PHONE_NUMBER_${TG_CHAT_ID}`];
+  }
+
+  return `${phoneNumber}_${NTFY_BASE_ID}`;
 };
 
 export default getCurrentUserNtfyID;
