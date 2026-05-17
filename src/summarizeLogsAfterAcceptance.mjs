d766e3f422985b55d53e3808e131fb8b807dbf9c
AhmedNassar147 <nassarahmed750@gmail.com>
2026-05-17 16:44:20 +0300

    ignore member when no cchi data

--- src/summarizeLogsAfterAcceptance.mjs
+++ src/summarizeLogsAfterAcceptance.mjs
@@ -232,4 +232,4 @@ export async function migrateLogWidths(referralEndTimestamp) {
   createConsoleMessage(`✅ Log migrated → ${file}`, "info");
 }
 
-await migrateLogWidths(1779014471000);
+// await migrateLogWidths(1779014471000);
