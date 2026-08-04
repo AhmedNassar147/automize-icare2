71afa1744903a05d5242c1622c686195b2a8fdca
AhmedNassar147 <nassarahmed750@gmail.com>
2026-08-04 13:35:09 +0300

    fix

--- src/getExtraTimeBasedLogs.mjs
+++ src/getExtraTimeBasedLogs.mjs
@@ -622,7 +622,7 @@ const getExtraTimeBasedLogs = async ({
         currentWait = -2;
 
         if (waitBucket === "far") {
-          currentWait += -2;
+          currentWait += isLikeDangerZone ? -4 : -2;
         }
       }
 
